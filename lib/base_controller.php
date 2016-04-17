01cf746a497da44a7c47cded21f5cb2189966c8d
0141456267 <timo.hautamaki@hotmail.com>
2016-04-17 17:20:17 +0300

    user logged in ei toimi

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -10,7 +10,7 @@
       $opiskelija_id = $_SESSION['opiskelija'];
        //Pyydetään Opiskelija-mallilta opiskelija session mukaisella id:llä
       $opiskelija = Opiskelija::find($opiskelija_id);
-
+      Kint::dump($opiskelija);
       return $opiskelija;
     }
 
